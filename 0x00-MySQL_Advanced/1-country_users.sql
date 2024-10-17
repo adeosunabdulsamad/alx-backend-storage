ca9dc59e9738e98cb491aea49f9aa16818041f7b
Adeosun Abdulsamad <adeosunabdulsamad25@gmail.com>
2024-10-17 21:12:11 +0000

    Ensured all keywords were in capital letters

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,9 +1,10 @@
 -- 3 first students in the Batch ID=3
 -- because Batch 3 is the besti!
 CREATE TABLE IF NOT EXISTS users (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255),
-    country enum('US', 'CO', 'TN') NOT NULL DEFAULT 'US',
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US',
+    PRIMARY KEY(id)
 );
 
