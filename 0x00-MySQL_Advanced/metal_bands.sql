8f9025a3299da233ccb173c36ac29e63fd6063ee
Abdulsamad Adeosun <59876143+adeosunabdulsamad@users.noreply.github.com>
2024-10-18 05:24:54 +0100

    Update metal_bands.sql

--- 0x00-MySQL_Advanced/metal_bands.sql
+++ 0x00-MySQL_Advanced/metal_bands.sql
@@ -13,7 +13,7 @@ DROP TABLE IF EXISTS `metal_bands`;
 CREATE TABLE `metal_bands` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `band_name` varchar(255) DEFAULT NULL,
-  `fans` int(11) DEFAULT NULL,
+  `nb_fans` int(11) DEFAULT NULL,
   `formed` year DEFAULT NULL,
   `origin` varchar(255) DEFAULT NULL,
   `split` year DEFAULT NULL,
@@ -21,7 +21,7 @@ CREATE TABLE `metal_bands` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-INSERT INTO `metal_bands` (`band_name`, `fans`, `formed`, `id`, `origin`, `split`, `style`)
+INSERT INTO `metal_bands` (`band_name`, `nb_fans`, `formed`, `id`, `origin`, `split`, `style`)
 VALUES ('Iron Maiden', '4195', '1975', '1', 'United Kingdom', NULL, 'New wave of british heavy,Heavy'),
 ('Opeth', '4147', '1990', '2', 'Sweden', '1990', 'Extreme progressive,Progressive rock,Progressive'),
 ('Metallica', '3712', '1981', '3', 'USA', NULL, 'Heavy,Bay area thrash'),
